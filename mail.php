72f807851d60b45b41bbaef963e2c8bbd97cb902
Данил Зенин <zenindanil2000@gmail.com>
2024-06-04 19:54:05 +0300

    site

--- mail.php
+++ mail.php
@@ -1,11 +1,27 @@
 <?php
-$name = $_GET['name'];
-$phone = $_GET['phone'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
 
-$to_email = 'zenindanil2000@gmail.com';
-$subject = 'Даня, пиши код сам';
-$message = 'Даня, данные пришли, смотри: ' + $name + 'Это - его имя, а это - ' + $phone + 'Его номер теелфонра. Вызвывай на него психушку';
-$headers = 'From: progulkinakatere@gmail.com';
-mail($to_email,$subject,$message,$headers);
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+
+$name = urldecode($name);
+$phone = urldecode($phone);
+
+$name = trim($name);
+$phone = trim($phone);
+
+
+if (mail("zenindanil2000@gmail.com",
+        "Новая заявка",
+        "Имя: ".$name."\n".
+        "Телефон: ".$phone,
+        "From: zenindanil2004@gmail.com \r\n")
+) {
+    echo("Письмо отправлено!");
+}
+else {
+    echo("Есть ошибки! Проверьте данные...");
+}
 
 ?>
