1e1433a74cb8216bc1853d0e8b0a8e219c22510c
Danil Zenin <zenindanil2000@gmail.com>
2025-09-10 15:42:26 +0300

    add data attributes in js

--- 3-1 Веб-программирование/lab1/js/main.js
+++ 3-1 Веб-программирование/lab1/js/main.js
@@ -4,7 +4,7 @@ const cart = JSON.parse(localStorage.getItem('cart')) || {};
 
 buttons.forEach(btn => {
     const id = btn.dataset.id;
-    const count = cart[id] || 0;
+    const count = cart[id]?.count || 0;
     btn.dataset.count = String(count);
 
     if (count > 0) {
@@ -18,28 +18,33 @@ buttons.forEach(btn => {
 });
 
 function increment(btn, cart) {
-  const id = btn.dataset.id;
+    const card = btn.closest('.products__item');
+    const id = btn.dataset.id;
+    const title = card.dataset.title;
+    const price = card.dataset.price;
+    const img = card.dataset.img;
 
-  if (!btn.classList.contains('is-active')) {
-    btn.classList.add('is-active');
-  }
+    if (!btn.classList.contains('is-active')) {
+        btn.classList.add('is-active');
+    }
 
-  let count = parseInt(btn.dataset.count || '0', 10) + 1;
-  btn.dataset.count = String(count);
-  cart[id] = count;
+    let count = parseInt(btn.dataset.count || '0', 10) + 1;
+    btn.dataset.count = String(count);
 
-  localStorage.setItem('cart', JSON.stringify(cart));
+    cart[id] = { id, title, price, img, count };
 
-  renderCounter(btn, count);
-}
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    renderCounter(btn, count);
+    }
 
 function renderCounter(btn, count) {
-  let counter = btn.querySelector('.products__itemBottomCount');
-  if (!counter) {
-    counter = document.createElement('span');
-    counter.className = 'products__itemBottomCount';
-    const textP = btn.querySelector('p');
-    btn.insertBefore(counter, textP);
-  }
-  counter.textContent = count;
+    let counter = btn.querySelector('.products__itemBottomCount');
+    if (!counter) {
+        counter = document.createElement('span');
+        counter.className = 'products__itemBottomCount';
+        const textP = btn.querySelector('p');
+        btn.insertBefore(counter, textP);
+    }
+    counter.textContent = count;
 }
